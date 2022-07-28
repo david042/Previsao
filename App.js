17ad393815d62050d987b729c3ceba1c6f3c8b60
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-07-28 15:07:18 -0300

    ainda não funciona

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import axios from 'axios';
 
 export default function App() {
+  const [tex, setTex] = useState(0);
+  const [texx, setTexx] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.texto}> Previsao do tempo </Text>
+        
+        <TextInput
+          style={styles.input}
+          placeholder='Digite aqui sua cidade'
+          keyboardType='text'
+          value={tex}
+          onChangeText={()=>axios.get(`https://api.hgbrasil.com/weather?key=203f1c51&${tex}`).then((response)=>setTexx(response.data))}
+        />
+        <TouchableOpacity
+         style={styles.botao}
+         //onPress={()=>axios.get('https://api.hgbrasil.com/weather?key=203f1c51').then((response)=>setValor(response.data.USDBRL))}
+        >
+        <Text style={styles.textoBotao}> Buscar </Text>
+        </TouchableOpacity>
+        
+        <Text>{texx}</Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +38,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto:{
+    fontSize: 30,
+    alignSelf: 'center'
+  },
+  botao:{
+    width: '50%',
+    alignSelf: 'center',
+    borderWidth: 1,
+    fontSize: 30,
+    borderRadius: 6,
+    backgroundColor: '#000',
+    marginBottom: 10,
+    textAlign: 'center'
+  },
+  textoBotao:{
+    color: '#fff',
+    fontSize: 30
+  },
+  input:{
+    width: '80%',
+    alignSelf: 'center',
+    borderWidth: 1,
+    fontSize: 30,
+    borderRadius: 6,
+    backgroundColor: '#000',
+    color: '#fff',
+    marginTop: 10,
+    marginBottom: 10
+  }
 });
