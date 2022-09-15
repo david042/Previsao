9c8af1eeefe6d56e63ad5eb0eed999485c6075d0
David042 <david.mal04@hotmail.com>
2022-09-15 14:45:02 -0300

    flatlist

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import Tempo from './components/tempo';
 import Api from './components/api';
 
@@ -8,31 +8,41 @@ export default function App() {
   const [cidade, setCidade] = useState("Mongaguá");
 
   async function carregaDados(){
-    const response = await Api.get(`weather?array_limit=2&fields=only_results,city_name,temp,date,time,description,forecast,min,max&key=2cf6d53f&city_name=${cidade}`)
-    setDados(response.data); 
+    const response = await Api.get(`weather?array_limit=10&fields=only_results,city_name,temp,date,time,description,forecast,min,max&key=2cf6d53f&city_name=${cidade}`)
+    setDados(response.data.forecast); 
   }
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text style={styles.texto}> Previsao do tempo </Text>
-        
-        <TextInput
-          style={styles.input}
-          placeholder='Digite aqui sua cidade'
-          keyboardType='default'
-          onChangeText={(value)=>setCidade(value)}
-        />
-        <TouchableOpacity
-         style={styles.botao}
-         onPress={carregaDados}
-        >
-        <Text style={styles.textoBotao}> Buscar </Text>
-        </TouchableOpacity>
-        <Text>
-          <Tempo data={dados}/>
-        </Text>
-      </View>
+      <Text style={styles.texto}> Previsao do tempo </Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Digite aqui sua cidade'
+        keyboardType='default'
+        onChangeText={(value)=>setCidade(value)}
+      />
+      <TouchableOpacity
+        style={styles.botao}
+        onPress={carregaDados}
+      >
+      <Text style={styles.textoBotao}> Buscar </Text>
+      </TouchableOpacity>
+      {/*<Text>
+        <Tempo data={dados}/>
+      </Text>*/}
+      <FlatList
+        data={dados}
+        renderItem={({item})=>{
+          return(
+            <View style={styles.previsao}>
+              <Text> Data: {item.date} </Text>
+              <Text> Max: {item.max} </Text>
+              <Text> Min: {item.min} </Text>
+              <Text> Descrição: {item.description} </Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
@@ -72,5 +82,9 @@ const styles = StyleSheet.create({
     color: '#fff',
     marginTop: 10,
     marginBottom: 10
+  },
+  previsao:{
+    width: '100%',
+    fontSize: 30
   }
 });
